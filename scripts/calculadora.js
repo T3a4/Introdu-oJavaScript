8ccb9e2f7c217245b24bc95825c4885eb1ef51e0
T3a4 <tabatha.mfrsouza@senacsp.edu.br>
2025-06-25 10:53:46 -0300

    Ajustar a reorganização da pasta

--- scripts/calculadora.js
+++ scripts/calculadora.js
@@ -1,32 +1,19 @@
 function somar() {
-    const num1 = parseFloat(document.getElementById('soma-num1').value);
-    const num2 = parseFloat(document.getElementById('soma-num2').value);
-    const resultado = num1 + num2;
-    document.getElementById('resultado-soma').textContent = `Resultado: ${resultado}`;
+    const { num1, num2 } = pegarValores();
+    document.getElementById('resultado').innerText = `Resultado: ${num1 + num2}`; 
 }
 
 function subtrair() {
-    const num1 = parseFloat(document.getElementById('sub-num1').value);
-    const num2 = parseFloat(document.getElementById('sub-num2').value);
-    const resultado = num1 - num2;
-    document.getElementById('resultado-sub').textContent = `Resultado: ${resultado}`;
+    const { num1, num2 } = pegarValores();
+    document.getElementById('resultado').innerText = `Resultado: ${num1 - num2}`;
 }
 
 function multiplicar() {
-    const num1 = parseFloat(document.getElementById('mult-num1').value);
-    const num2 = parseFloat(document.getElementById('mult-num2').value);
-    const resultado = num1 * num2;
-    document.getElementById('resultado-mult').textContent = `Resultado: ${resultado}`;
+    const { num1, num2 } = pegarValores();
+    document.getElementById('resultado').innerText = `Resultado: ${num1 * num2}`;
 }
 
 function dividir() {
-    const num1 = parseFloat(document.getElementById('div-num1').value);
-    const num2 = parseFloat(document.getElementById('div-num2').value);
-
-    if (num2 === 0) {
-        document.getElementById('resultado-div').textContent = 'Erro: divisão por zero';
-    } else {
-        const resultado = num1 / num2;
-        document.getElementById('resultado-div').textContent = `Resultado: ${resultado}`;
-    }
+    const { num1, num2 } = pegarValores();
+    document.getElementById('resultado').innerText = `Resultado: ${num1 + num2}`;
 }
